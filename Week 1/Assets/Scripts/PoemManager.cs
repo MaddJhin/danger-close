4e5fcd9966ad07087606b27a9a84a3cf10ee0f34
fc1159 <fc1159@nyu.edu>
2014-07-14 20:44:54 -0400

    Week one homework handin

---
+++ Week 1/Assets/Scripts/PoemManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello Worl!");
+
+		GetComponent<TextMesh> ().text = "Home is where the heart is,\n" +
+			"home is where the fart is.\n" +
+			"Come let us fart in the home.\n" +
+			"There is no art in a fart.\n" +
+			"Still a fart may not be artless.\n" +
+			"Let us fart and artless fart in the home.\n" +
+			"- Ernest Hemingway" ;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

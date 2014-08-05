31bfe3512d00811c5c17b8d0472c6d8b10520bd7
fc1159 <fc1159@nyu.edu>
2014-08-05 12:34:05 -0400

    Stuff

--- Mid Term/Assets/Scripts/ObjectInterraction.cs
+++ Mid Term/Assets/Scripts/ObjectInterraction.cs
@@ -13,6 +13,7 @@ public class ObjectInterraction : MonoBehaviour {
 	bool haveLightBulb = false;
 	bool doorClosed = true;
 	bool gameOver = false;
+	bool doorChecked = false;
 
 	string textBuffer = "A ship is about to crash into the rocks and the bulb is broken!\n" +
 		"Find the replacement bulb and install it to save the day.";
@@ -21,31 +22,32 @@ public class ObjectInterraction : MonoBehaviour {
 	void Update () {
 	
 		//Construct a ray (an origin and a direction)
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-		//initialize a "RaycastHit" info container
-		RaycastHit rayHit = new RaycastHit(); //empty, blank
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
-		if (Physics.Raycast (ray, out rayHit, playerReach) && Input.GetMouseButtonDown (0)) {
+			//initialize a "RaycastHit" info container
+			RaycastHit rayHit = new RaycastHit(); //empty, blank
 
-			if (rayHit.collider.gameObject == spareKeyHolder && haveKey == false) {
+			if (Physics.Raycast (ray, out rayHit, playerReach) && Input.GetMouseButtonDown (0)) {
+
+			if (rayHit.collider.gameObject == spareKeyHolder && haveKey == false && doorChecked == true) {
 				rayHit.transform.Translate (-1,0,0);
 				haveKey = true;
 				textBuffer = "You have found the spare key.";
 			}
 
-			else if (rayHit.collider.name == "Door" && haveKey == false) {
+			else if (rayHit.collider.name == door.name && haveKey == false) {
 				textBuffer = "You locked yourself out!" +
 					"\nIf only you had a spare key.... " +
-					"\nperhaps hidden under some inconspicuous \"rock\".";
+					"\nperhaps hidden under some inconspicuous rock.";
+				doorChecked = true;
 			}
 
-			else if (rayHit.collider.name == "Door" && haveKey == true && doorClosed == true) {
+			else if (rayHit.collider.name == door.name && haveKey == true && doorClosed == true) {
 				rayHit.collider.transform.Rotate (0, 90, 0);
 				doorClosed = false;
 			}
 
-			else if (rayHit.collider.name == "Door" && haveKey == true && doorClosed == false) {
+			else if (rayHit.collider.name == door.name && haveKey == true && doorClosed == false) {
 				rayHit.collider.transform.Rotate (0, -90, 0);
 				doorClosed = true;
 			}
---
+++ Week 4/Assets/Scripts/procGenBasic.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class procGenBasic : MonoBehaviour {
+
+	public Transform small, medium, big, tall;
+	public float spawnRadius = 100;
+	public float spawnCount = 1000;
+	public float spawnHeight = 10;
+
+	public Transform[] prefabs; //an array is a list of things in one varialbe
+
+	// Use this for initialization
+	void Start () {
+	
+		int counter = 0;
+
+		while (counter < spawnCount) {
+			Transform prefabToSpawn; //starts blank
+			int prefabIndex = Random.Range (0,4);
+
+			prefabToSpawn = prefabs [prefabIndex];
+//			if (prefabIndex == 0){
+//				prefabToSpawn = small;
+//			}
+//			else if (prefabIndex == 1) {
+//				prefabToSpawn = medium;
+//			}
+//			else if (prefabIndex == 2) {
+//				prefabToSpawn = big;
+//			}
+//			else {
+//				prefabToSpawn = tall;
+//			}
+
+			Vector3 randomVector = new Vector3(Random.Range (-spawnRadius, spawnRadius), Random.Range (0,spawnHeight), Random.Range (-spawnRadius, spawnRadius));
+			Instantiate (prefabToSpawn, randomVector, Quaternion.identity);
+			counter ++; // add 1 to "counter" after each loop
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

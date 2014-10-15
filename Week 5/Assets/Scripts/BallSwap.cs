279af3f1de34c2f232f5020c210cf0c737e08509
Francisco Carrera <franciscocarrer@gmail.com>
2014-10-15 09:27:44 -0400

    File Cleanup

---
+++ Week 5/Assets/Scripts/BallSwap.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSwap : MonoBehaviour {
+
+	public Transform ball1, ball2; // assign in the inspector
+	public float shakeSpeed = 10;
+	public float shakeDuration = 5f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (BallCoroutine ());
+	}
+
+	IEnumerator BallCoroutine () {
+//		Debug.Log ("Corutine started");
+//		yield return 0; //wait one frame
+//		Debug.Log ("A frame elapsed");
+//		yield return new WaitForSeconds (3.4f);
+//		Debug.Log ("Now 3.4 seconds have elapsed");
+
+		//Lerp with while () loops
+
+		while (true) {
+			//Remember the starting ball positions
+			Vector3 ball1Pos = ball1.position;
+			Vector3 ball2Pos = ball2.position; 
+			float t = 0f;
+			bool soundPlayed = false; 
+			while (t < 1) { // lerp as long as t is less than one
+				t += Time.deltaTime;
+				if (t >= 0.5 && !soundPlayed) {
+					audio.Play ();
+					StartCoroutine ( ScreenShake (shakeDuration, shakeSpeed));
+					soundPlayed = true;
+				}
+				ball1.position = Vector3.Lerp (ball1Pos, ball2Pos, t);
+				ball2.position = Vector3.Lerp (ball2Pos, ball1Pos, t);
+				yield return 0;
+			}
+			yield return 0;
+		}
+	}
+
+	IEnumerator ScreenShake (float duration, float speed) {
+		float t = 1f; // Value from 0 to 1
+		Vector3 cameraStartPos = Camera.main.transform.position;
+
+		while (t > 0 ) {
+			t -= Time.deltaTime / duration;
+			Camera.main.transform.position = cameraStartPos + Camera.main.transform.right * Mathf.Sin (Time.time * speed) * t +
+															  Camera.main.transform.up * Mathf.Sin (Time.time * speed * 1.1f) * t;
+			yield return 0;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Week 5/Assets/Scripts/SineWaveDemo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineWaveDemo : MonoBehaviour {
+
+	public float speed =  5f;
+	public float height = 1f;
+	public float degrees = 60f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.zero + new Vector3(0f, 1f, 0f) * Mathf.Sin(Time.time * speed) * height;
+		transform.localEulerAngles = new Vector3 (Mathf.Sin (Time.time) * degrees, 0, 0f);
+	}
+}
